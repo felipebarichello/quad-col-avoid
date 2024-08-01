ec20c3fb18281ba2397eb31fa1062f8bd800cb0e
Felipe Barichello <87396379+felipebarichello@users.noreply.github.com>
2024-08-01 17:46:11 -0300

    Make processes.py use PX4-Autopilot-ColAvoid

--- src/ROS2_PX4_Offboard/px4_offboard/px4_offboard/processes.py
+++ src/ROS2_PX4_Offboard/px4_offboard/px4_offboard/processes.py
@@ -11,7 +11,7 @@ commands = [
     "MicroXRCEAgent udp4 -p 8888",
 
     # Run the PX4 SITL simulation
-    "cd ~/PX4-Autopilot && make px4_sitl gz_x500_lidar"
+    "cd ~/PX4-Autopilot-ColAvoid && make px4_sitl gz_x500_lidar"
 
     # Run QGroundControl
     # "cd ~/QGroundControl && ./QGroundControl.AppImage"
